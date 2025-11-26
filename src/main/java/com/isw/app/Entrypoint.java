0fce8cb742750d3ab50ad250037d8b53de31d7a6
roney ruiz <roneyruizrojas@gmail.com>
2025-11-26 11:55:13 -0600

    feat: adjust setup files to launch the app

---
+++ src/main/java/com/isw/app/Entrypoint.java
@@ -0,0 +1,19 @@
+package com.isw.app;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+
+public class Entrypoint extends Application {
+
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(Entrypoint.class.getResource("/presentation/register-view.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    stage.setTitle("Hello!");
+    stage.setScene(scene);
+    stage.show();
+  }
+}
--- src/main/java/com/isw/app/Launcher.java
+++ src/main/java/com/isw/app/Launcher.java
@@ -3,7 +3,7 @@ package com.isw.app;
 import javafx.application.Application;
 
 public class Launcher {
-    static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
+  public static void main(String[] args) {
+    Application.launch(Entrypoint.class, args);
+  }
 }
