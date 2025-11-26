7dad4e5c347ff20087d46ded82b797994b570bc9
roney ruiz <roneyruizrojas@gmail.com>
2025-11-25 20:52:12 -0600

    feat: setup maven project

---
+++ src/main/java/com/isw/app/Launcher.java
@@ -0,0 +1,9 @@
+package com.isw.app;
+
+import javafx.application.Application;
+
+public class Launcher {
+    static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
