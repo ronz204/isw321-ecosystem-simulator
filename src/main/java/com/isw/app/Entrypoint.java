bb896d3898185dd88787b5b5969dfae0bc734968
roney ruiz <roneyruizrojas@gmail.com>
2025-11-26 11:58:34 -0600

    feat: add login controller and change views to presenters

--- src/main/java/com/isw/app/Entrypoint.java
+++ src/main/java/com/isw/app/Entrypoint.java
@@ -10,7 +10,7 @@ public class Entrypoint extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(Entrypoint.class.getResource("/presentation/register-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(Entrypoint.class.getResource("/presentation/register-presenter.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 320, 240);
     stage.setTitle("Hello!");
     stage.setScene(scene);
