61a2e6b24741e14424a77b8c8089e56dc30f8704
roney ruiz <roneyruizrojas@gmail.com>
2025-11-26 11:55:04 -0600

    fix: java reflection for javafx in controllers

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.isw.app.isw321ecosystemsimulator {
+module com.isw.app {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
 
-    opens com.isw.app to javafx.fxml;
+    opens com.isw.app.controllers to javafx.fxml;
     exports com.isw.app;
 }
