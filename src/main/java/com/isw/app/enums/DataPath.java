7332b27cb4b3baa632bbe292a1a64e21a968605c
roney ruiz <roneyruizrojas@gmail.com>
2025-11-27 20:54:58 -0600

    feat: credata data path enum for txt files

---
+++ src/main/java/com/isw/app/enums/DataPath.java
@@ -0,0 +1,18 @@
+package com.isw.app.enums;
+
+import java.nio.file.Paths;
+
+public enum DataPath {
+  CUSTOMERS("customers.txt"),
+  ECOSYSTEMS("ecosystems.txt");
+
+  private final String filename;
+
+  DataPath(String filename) {
+    this.filename = filename;
+  }
+
+  public String getFilename() {
+    return Paths.get("src", "main", "java", "com", "isw", "app", "database", filename).toString();
+  }
+}
