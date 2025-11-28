1b587d640f9515edb7650a556c03cafc8291423e
roney ruiz <roneyruizrojas@gmail.com>
2025-11-27 20:56:53 -0600

    feat: update registrer presenter with more fields

--- src/main/java/com/isw/app/presentation/RegisterPresenter.java
+++ src/main/java/com/isw/app/presentation/RegisterPresenter.java
@@ -1,22 +1,39 @@
 package com.isw.app.presentation;
 
 import javafx.fxml.FXML;
+import java.time.LocalDate;
 import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
+
 import com.isw.app.helpers.ValidatorHelper;
-import com.isw.app.handlers.register.RegisterSchema;
-import com.isw.app.handlers.register.RegisterHandler;
-import com.isw.app.handlers.register.RegisterResponse;
+import com.isw.app.repositories.customer.CustomerRepository;
+import com.isw.app.repositories.customer.CustomerTLQRepository;
+import com.isw.app.handlers.customer.register.RegisterCustomerSchema;
+import com.isw.app.handlers.customer.register.RegisterCustomerHandler;
+import com.isw.app.handlers.customer.register.RegisterCustomerCommand;
+import com.isw.app.handlers.customer.register.RegisterCustomerResponse;
 
 public class RegisterPresenter {
-  private final RegisterHandler registerHandler = new RegisterHandler();
+  private final CustomerRepository repository = new CustomerTLQRepository();
+  private final RegisterCustomerHandler handler = new RegisterCustomerHandler(repository);
 
   @FXML
-  private TextField usernameField;
+  private TextField nameField;
 
   @FXML
   private TextField emailField;
+
+  @FXML
+  private TextField cedulaField;
+
+  @FXML
+  private ComboBox<String> genderField;
+
+  @FXML
+  private DatePicker birthdayField;
 
   @FXML
   private PasswordField passwordField;
@@ -26,11 +43,16 @@ public class RegisterPresenter {
 
   @FXML
   public void onRegisterClick() {
-    String username = usernameField.getText();
+    String name = nameField.getText();
     String email = emailField.getText();
+    String cedula = cedulaField.getText();
+    String gender = genderField.getValue();
     String password = passwordField.getText();
+    LocalDate birthday = birthdayField.getValue();
 
-    RegisterSchema schema = new RegisterSchema(username, email, password);
+    RegisterCustomerSchema schema = new RegisterCustomerSchema(
+        cedula, name, email, gender, password, birthday);
+
     var violations = ValidatorHelper.validate(schema);
 
     if (!violations.isEmpty()) {
@@ -39,9 +61,11 @@ public class RegisterPresenter {
       return;
     }
 
-    RegisterResponse response = registerHandler.handler(schema.toCommand());
-    
+    RegisterCustomerCommand command = schema.toCommand();
+    RegisterCustomerResponse response = handler.handle(command);
+
     messageLabel.setText(response.message());
-    messageLabel.setStyle("-fx-text-fill: green;");
+    messageLabel.setStyle(
+        response.message().startsWith("Cliente registrado") ? "-fx-text-fill: green;" : "-fx-text-fill: red;");
   }
 }
