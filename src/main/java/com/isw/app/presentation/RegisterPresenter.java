139137d760ac695509745e38ede90fb5b7d163ed
roney ruiz <roneyruizrojas@gmail.com>
2025-11-28 20:18:56 -0600

    feat: improve ui design of register presenter

--- src/main/java/com/isw/app/presentation/RegisterPresenter.java
+++ src/main/java/com/isw/app/presentation/RegisterPresenter.java
@@ -3,10 +3,11 @@ package com.isw.app.presentation;
 import javafx.fxml.FXML;
 import java.time.LocalDate;
 import javafx.scene.control.Label;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 import com.isw.app.helpers.ValidatorHelper;
 import com.isw.app.repositories.customer.CustomerRepository;
@@ -30,9 +31,6 @@ public class RegisterPresenter {
   private TextField cedulaField;
 
   @FXML
-  private ComboBox<String> genderField;
-
-  @FXML
   private DatePicker birthdayField;
 
   @FXML
@@ -42,11 +40,24 @@ public class RegisterPresenter {
   private Label messageLabel;
 
   @FXML
+  private RadioButton maleRadio;
+
+  @FXML
+  private RadioButton femaleRadio;
+
+  @FXML
+  private ToggleGroup genderGroup;
+
+  @FXML
   public void onRegisterClick() {
     String name = nameField.getText();
     String email = emailField.getText();
     String cedula = cedulaField.getText();
-    String gender = genderField.getValue();
+    String gender = null;
+    if (maleRadio.isSelected())
+      gender = "Masculino";
+    else if (femaleRadio.isSelected())
+      gender = "Femenino";
     String password = passwordField.getText();
     LocalDate birthday = birthdayField.getValue();
 
@@ -65,7 +76,20 @@ public class RegisterPresenter {
     RegisterCustomerResponse response = handler.handle(command);
 
     messageLabel.setText(response.message());
-    messageLabel.setStyle(
-        response.message().startsWith("Cliente registrado") ? "-fx-text-fill: green;" : "-fx-text-fill: red;");
+    if (response.message().startsWith("Cliente registrado")) {
+      messageLabel.setStyle("-fx-text-fill: #388e3c;");
+      clearFields();
+    } else {
+      messageLabel.setStyle("-fx-text-fill: red;");
+    }
+  }
+
+  private void clearFields() {
+    nameField.clear();
+    emailField.clear();
+    cedulaField.clear();
+    birthdayField.setValue(null);
+    passwordField.clear();
+    genderGroup.selectToggle(null);
   }
 }
