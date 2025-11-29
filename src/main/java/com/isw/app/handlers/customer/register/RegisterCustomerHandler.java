f889a1801d298e741e12cce5d21a1e35c7afba99
roney ruiz <roneyruizrojas@gmail.com>
2025-11-28 19:39:47 -0600

    feat: add age validation to register customer handler

--- src/main/java/com/isw/app/handlers/customer/register/RegisterCustomerHandler.java
+++ src/main/java/com/isw/app/handlers/customer/register/RegisterCustomerHandler.java
@@ -2,6 +2,7 @@ package com.isw.app.handlers.customer.register;
 
 import com.isw.app.models.Customer;
 import com.isw.app.helpers.HashingHelper;
+import com.isw.app.helpers.LocalDateHelper;
 import com.isw.app.repositories.customer.CustomerRepository;
 
 public class RegisterCustomerHandler {
@@ -18,6 +19,10 @@ public class RegisterCustomerHandler {
 
     if (repository.findByCedula(command.cedula()).isPresent()) {
       return new RegisterCustomerResponse("Ya existe un cliente con esa cédula.");
+    }
+
+    if (!LocalDateHelper.hasRequiredAge(command.birthday(), 18)) {
+      return new RegisterCustomerResponse("El cliente debe ser mayor de edad.");
     }
 
     Customer customer = command.toEntity();
