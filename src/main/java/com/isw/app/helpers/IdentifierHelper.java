85ae23afe1a90e8b563d88e67e6bbbdfdab6f99a
roney ruiz <roneyruizrojas@gmail.com>
2025-11-30 16:34:28 -0600

    chore: remove bad identifier helper and adjust ecosystem model

--- src/main/java/com/isw/app/helpers/IdentifierHelper.java
+++
@@ -1,8 +0,0 @@
-package com.isw.app.helpers;
-
-public class IdentifierHelper {
-  public static String generate(String prefix) {
-    int number = RandomHelper.getRandomInt(1000, 9999);
-    return String.format("%s-%04d", prefix, number);
-  }
-}
--- src/main/java/com/isw/app/models/Ecosystem.java
+++ src/main/java/com/isw/app/models/Ecosystem.java
@@ -1,23 +1,15 @@
 package com.isw.app.models;
 
 import com.isw.app.enums.EcosystemType;
-import com.isw.app.helpers.IdentifierHelper;
 
 public class Ecosystem {
-  private final String PREFIX = "ECO";
 
-  private final String id;
   private EcosystemType type;
   private Dashboard dashboard;
 
   public Ecosystem(EcosystemType type) {
-    this.id = IdentifierHelper.generate(PREFIX);
     this.dashboard = new Dashboard();
     this.type = type;
-  }
-
-  public String getId() {
-    return id;
   }
 
   public EcosystemType getType() {
