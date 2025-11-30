3ff243b502986dc506e593369fd7431114c36775
roney ruiz <roneyruizrojas@gmail.com>
2025-11-30 14:55:30 -0600

    chore: update entrypoint to use the switcher helper

--- src/main/java/com/isw/app/Entrypoint.java
+++ src/main/java/com/isw/app/Entrypoint.java
@@ -1,19 +1,16 @@
 package com.isw.app;
 
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-import javafx.fxml.FXMLLoader;
+import com.isw.app.enums.FXMLPath;
 import javafx.application.Application;
+import com.isw.app.helpers.SwitcherHelper;
 
 public class Entrypoint extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(Entrypoint.class.getResource("/presentation/register-presenter.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    stage.setTitle("Hello!");
-    stage.setScene(scene);
-    stage.show();
+    SwitcherHelper.setStage(stage);
+    SwitcherHelper.switchTo(FXMLPath.LOGIN.getPath());
   }
 }
