8ce75e8917ae6b7d43505a2ee29f15a5b237e1bf
roney ruiz <roneyruizrojas@gmail.com>
2025-11-30 17:33:41 -0600

    feat: return identifier helper and add uuid to animals

--- src/main/java/com/isw/app/core/objects/Animal.java
+++ src/main/java/com/isw/app/core/objects/Animal.java
@@ -1,15 +1,23 @@
 package com.isw.app.core.objects;
 
 import com.isw.app.enums.AnimalDetail;
+import com.isw.app.helpers.IdentifierHelper;
 
 public class Animal {
+  private final String PREFIX = "ANI";
   
+  private String uuid;
   private Coord coord;
   private AnimalDetail detail;
 
   public Animal(AnimalDetail detail, Coord coord) {
+    this.uuid = IdentifierHelper.generate(PREFIX);
     this.detail = detail;
     this.coord = coord;
+  }
+
+  public String getUuid() {
+    return uuid;
   }
 
   public Coord getCoord() {
