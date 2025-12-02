f480baac4893482627c4696890d30a012bde450c
roney ruiz <roneyruizrojas@gmail.com>
2025-12-01 23:42:09 -0600

    chore: adjust animal factory with specific behaviors

--- src/main/java/com/isw/app/core/factories/AnimalFactory.java
+++ src/main/java/com/isw/app/core/factories/AnimalFactory.java
@@ -4,16 +4,21 @@ import com.isw.app.core.objects.Coord;
 import com.isw.app.core.objects.Matrix;
 import com.isw.app.core.objects.Animal;
 import com.isw.app.core.objects.Detail;
+import com.isw.app.core.behavior.Behavior;
+import com.isw.app.core.behavior.CarnivoreBehavior;
+import com.isw.app.core.behavior.HerbivoreBehavior;
 
 public class AnimalFactory {
 
   public static Animal buildCarnivore() {
     Coord coord = Matrix.getRandomCoord();
-    return new Animal(Detail.CARNIVORE, coord);
+    Behavior behavior = new CarnivoreBehavior();
+    return new Animal(Detail.CARNIVORE, coord, behavior);
   }
 
   public static Animal buildHerbivore() {
     Coord coord = Matrix.getRandomCoord();
-    return new Animal(Detail.HERBIVORE, coord);
+    Behavior behavior = new HerbivoreBehavior();
+    return new Animal(Detail.HERBIVORE, coord, behavior);
   }
 }
