dec95af30736db2310dc7d4a8cf28b4aa880168f
roney ruiz <roneyruizrojas@gmail.com>
2025-12-04 09:07:28 -0600

    chore: purge behavior components temporaly

--- src/main/java/com/isw/app/core/objects/Animal.java
+++ src/main/java/com/isw/app/core/objects/Animal.java
@@ -11,55 +11,11 @@ public class Animal {
   private Detail detail;
   private Behavior behavior;
 
-  private int meals;
-  private int aliveTurns;
-  private int hungryTurns;
-
   public Animal(Detail detail, Coord coord, Behavior behavior) {
     this.uuid = IdentifierHelper.generate(PREFIX);
     this.behavior = behavior;
     this.detail = detail;
     this.coord = coord;
-
-    this.meals = 0;
-    this.aliveTurns = 0;
-    this.hungryTurns = 0;
-  }
-
-  public void act(Matrix matrix) {
-    Coord next = behavior.nextMove(this, matrix);
-    boolean ate = false;
-    if (next != null) {
-      ate = behavior.canEat(next, matrix);
-      coord = next;
-    }
-    behavior.update(this, ate);
-  }
-
-  public boolean isDead() {
-    return detail == Detail.CARNIVORE && hungryTurns >= 3;
-  }
-
-  public boolean canReproduce() {
-    return behavior.canReproduce(this);
-  }
-
-  public Animal reproduce() {
-    return new Animal(this.detail, this.coord, this.behavior);
-  }
-
-  public boolean isPrey() {
-    return detail == Detail.HERBIVORE;
-  }
-
-  public boolean isPredator() {
-    return detail == Detail.CARNIVORE;
-  }
-
-  public String toFileFormat() {
-    return String.format("%s,%s,%d,%d,%d,%d,%d",
-        uuid, detail, coord.getRow(), coord.getCol(),
-        aliveTurns, hungryTurns, meals);
   }
 
   public String getUuid() {
@@ -74,39 +30,7 @@ public class Animal {
     return detail;
   }
 
-  public int getAliveTurns() {
-    return aliveTurns;
-  }
-
-  public int getHungryTurns() {
-    return hungryTurns;
-  }
-
-  public int getMeals() {
-    return meals;
-  }
-
-  public void setCoord(Coord c) {
-    coord = c;
-  }
-
-  public void incAliveTurns() {
-    aliveTurns++;
-  }
-
-  public void incHungryTurns() {
-    hungryTurns++;
-  }
-
-  public void resetHungryTurns() {
-    hungryTurns = 0;
-  }
-
-  public void incMeals() {
-    meals++;
-  }
-
-  public void resetMeals() {
-    meals = 0;
+  public void setCoord(Coord coord) {
+    this.coord = coord;
   }
 }
