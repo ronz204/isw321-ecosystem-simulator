b1393abeb8c80480ccc6afe04a2d1d29cd61c533
roney ruiz <roneyruizrojas@gmail.com>
2025-12-06 00:47:36 -0600

    feat: create behaviors for different animals

--- src/main/java/com/isw/app/core/behavior/HerbivoreBehavior.java
+++ src/main/java/com/isw/app/core/behavior/HerbivoreBehavior.java
@@ -1,5 +1,45 @@
 package com.isw.app.core.behavior;
 
-public class HerbivoreBehavior extends Behavior {
+import com.isw.app.core.objects.Action;
+import com.isw.app.core.objects.Matrix;
+import com.isw.app.core.objects.Animal;
+import com.isw.app.core.objects.Result;
+import com.isw.app.core.attempts.AttemptMove;
+import com.isw.app.core.attempts.AttemptReproduce;
 
+public class HerbivoreBehavior extends Behavior {
+  private final int REPRODUCTION_THRESHOLD = 2;
+
+  @Override
+  public Result act(Animal animal, Matrix matrix) {
+
+    turnsAlive++;
+    turnsSinceLastSex++;
+
+    if (canReproduce()) {
+      Result reproduction = AttemptReproduce.execute(animal, matrix);
+
+      if (reproduction.getAction() == Action.REPRODUCE) {
+        turnsSinceLastSex = 0;
+        return reproduction;
+      }
+    }
+
+    return AttemptMove.execute(animal, matrix);
+  }
+
+  @Override
+  public boolean canEat() {
+    return false;
+  }
+
+  @Override
+  public boolean canDie() {
+    return false;
+  }
+
+  @Override
+  public boolean canReproduce() {
+    return turnsSinceLastSex >= REPRODUCTION_THRESHOLD;
+  }
 }
--- src/main/java/com/isw/app/core/behavior/OmnivoreBehavior.java
+++ src/main/java/com/isw/app/core/behavior/OmnivoreBehavior.java
@@ -1,5 +1,30 @@
 package com.isw.app.core.behavior;
 
+import com.isw.app.core.objects.Animal;
+import com.isw.app.core.objects.Matrix;
+import com.isw.app.core.objects.Result;
+
 public class OmnivoreBehavior extends Behavior {
+
+  @Override
+  public Result act(Animal animal, Matrix matrix) {
+    return Result.idle();
+  }
+
+
+  @Override
+  public boolean canEat() {
+    return true;
+  }
+
+  @Override
+  public boolean canDie() {
+    return true;
+  }
+
+  @Override
+  public boolean canReproduce() {
+    return true;
+  }
 
 }
