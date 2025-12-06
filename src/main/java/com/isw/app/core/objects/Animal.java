abfefa357b06f405c2e893e8df74d85abcb690a4
roney ruiz <roneyruizrojas@gmail.com>
2025-12-05 18:53:31 -0600

    chore: adjust animal model methods

--- src/main/java/com/isw/app/core/objects/Animal.java
+++ src/main/java/com/isw/app/core/objects/Animal.java
@@ -10,6 +10,7 @@ public class Animal {
   private Coord coord;
   private Detail detail;
   private Behavior behavior;
+  private boolean isDead = false;
 
   public Animal(Detail detail, Coord coord, Behavior behavior) {
     this.uuid = IdentifierHelper.generate(PREFIX);
@@ -22,15 +23,40 @@ public class Animal {
     return uuid;
   }
 
+  public boolean isDead() {
+    return isDead;
+  }
+
   public Coord getCoord() {
     return coord;
   }
+
+  public int getRow() {
+    return coord.getRow();
+  }
+
+  public int getCol() {
+    return coord.getCol();
+  }
+
   public String getLabel() {
     return detail.getLabel();
   }
 
+  public String getIcon() {
+    return detail.getIcon();
+  }
+
   public Detail getDetail() {
     return detail;
+  }
+
+  public Behavior getBehavior() {
+    return behavior;
+  }
+
+  public void makeAsDead() {
+    this.isDead = true;
   }
 
   public void setCoord(Coord coord) {
