f75d565c3f3356ebce8680302dd7fa39b3b6559c
roney ruiz <roneyruizrojas@gmail.com>
2025-12-05 18:28:15 -0600

    chore: update random helper with next int method

--- src/main/java/com/isw/app/helpers/RandomHelper.java
+++ src/main/java/com/isw/app/helpers/RandomHelper.java
@@ -8,4 +8,8 @@ public class RandomHelper {
   public static int getRandomInt(int min, int max) {
     return random.nextInt((max - min) + 1) + min;
   }
+
+  public static int getNextInt(int bound) {
+    return random.nextInt(bound);
+  }
 }
