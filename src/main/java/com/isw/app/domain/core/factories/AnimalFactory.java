e9238d0b6e2d0a44bad8f022106587799cc727b6
roney ruiz <roneyruizrojas@gmail.com>
2025-12-07 15:41:25 -0600

    fix: animal factory generate same behaviors

--- src/main/java/com/isw/app/domain/core/factories/AnimalFactory.java
+++ src/main/java/com/isw/app/domain/core/factories/AnimalFactory.java
@@ -1,7 +1,5 @@
 package com.isw.app.domain.core.factories;
 
-import java.util.Map;
-import java.util.HashMap;
 import com.isw.app.domain.core.objects.Coord;
 import com.isw.app.domain.core.objects.Detail;
 import com.isw.app.domain.core.objects.Animal;
@@ -11,16 +9,22 @@ import com.isw.app.domain.core.behaviors.HerbivoreBehavior;
 import com.isw.app.domain.core.behaviors.CarnivoreBehavior;
 
 public class AnimalFactory {
-  private static final Map<Detail, Behavior> behaviors = new HashMap<>();
-
-  static {
-    behaviors.put(Detail.OMNIVORE, new OmnivoreBehavior());
-    behaviors.put(Detail.HERBIVORE, new HerbivoreBehavior());
-    behaviors.put(Detail.CARNIVORE, new CarnivoreBehavior());
-  }
 
   public static Animal build(Detail detail, Coord coord) {
-    Behavior behavior = behaviors.get(detail);
+    Behavior behavior = getBehavior(detail);
     return new Animal(detail, coord, behavior);
+  }
+
+  private static Behavior getBehavior(Detail detail) {
+    switch (detail) {
+      case OMNIVORE:
+        return new OmnivoreBehavior();
+      case CARNIVORE:
+        return new CarnivoreBehavior();
+      case HERBIVORE:
+        return new HerbivoreBehavior();
+      default:
+        return null;
+    }
   }
 }
