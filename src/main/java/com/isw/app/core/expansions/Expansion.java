46fe2cd25a0446dfa6a217483f403bb9eb1a4d9b
roney ruiz <roneyruizrojas@gmail.com>
2025-12-07 01:41:15 -0600

    chore: purge fisrt core layer logic

--- src/main/java/com/isw/app/core/expansions/Expansion.java
+++
@@ -1,9 +0,0 @@
-package com.isw.app.core.expansions;
-
-import com.isw.app.core.artifacts.ContextArtifact;
-
-public abstract class Expansion {
-  public abstract String getName();
-  public abstract String getDescription();
-  public abstract void apply(ContextArtifact context);
-}
--- src/main/java/com/isw/app/core/expansions/mutations/MutationExpansion.java
+++
@@ -1,7 +0,0 @@
-package com.isw.app.core.expansions.mutations;
-
-import com.isw.app.core.expansions.Expansion;
-
-public abstract class MutationExpansion extends Expansion {
-
-}
