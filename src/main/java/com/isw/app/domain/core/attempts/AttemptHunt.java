0ff3c07d2c37977ab6ff512ab39479cdc1ac95ae
roney ruiz <roneyruizrojas@gmail.com>
2025-12-07 15:42:24 -0600

    feat: create attempt hunt

--- src/main/java/com/isw/app/domain/core/attempts/AttemptHunt.java
+++ src/main/java/com/isw/app/domain/core/attempts/AttemptHunt.java
@@ -1,5 +1,47 @@
 package com.isw.app.domain.core.attempts;
 
-public class AttemptHunt {
+import java.util.List;
+import java.util.stream.Collectors;
+import com.isw.app.domain.core.objects.Animal;
+import com.isw.app.domain.core.objects.Detail;
+import com.isw.app.domain.core.objects.Matrix;
+import com.isw.app.domain.core.objects.Result;
+import com.isw.app.application.helpers.RandomHelper;
+import com.isw.app.domain.core.setup.SimulatorContext;
+import com.isw.app.application.contexts.SimulationContext;
 
+public class AttemptHunt extends Attempt {
+
+  @Override
+  public Result execute(Animal animal, Matrix matrix) {
+    List<Animal> adjacentAnimals = matrix.getAdjacentAnimals(animal.getCoord());
+    
+    List<Animal> huntablePreys = adjacentAnimals.stream()
+        .filter(prey -> !prey.isDead() && prey.getTier() <= animal.getTier())
+        .collect(Collectors.toList());
+    
+    if (huntablePreys.isEmpty()) {
+      return Result.failed(animal);
+    }
+    
+    int index = RandomHelper.getChooseInt(huntablePreys.size());
+    Animal prey = huntablePreys.get(index);
+    
+    SimulationContext simulation = SimulationContext.getInstance();
+    SimulatorContext context = SimulatorContext.getInstance();
+    
+    Detail preyDetail = prey.getDetail();
+    context.getAnimals().get(preyDetail).remove(prey);
+    
+    if (context.getAnimals().get(preyDetail).isEmpty()) {
+      int turn = simulation.getCurrent();
+      context.getExtinctions().put(preyDetail, turn);
+    }
+    
+    prey.markAsDead();
+    context.getAnimals().get(Detail.CORPSE).add(prey);
+  
+    animal.getBehavior().resetMealTurns();
+    return Result.eat(animal, prey);
+  }
 }
